fc567a1a20a16760eb22ef4c80394c4cff9d9b96
Shuflduf <shuflduf@gmail.com>
2024-11-06 15:04:03 -0700

    wow

--- src/image_test.rs
+++ src/image_test.rs
@@ -1,7 +1,22 @@
+use reqwest::Method;
+
 use crate::Conversation;
+use std::fs::File;
 
 impl Conversation {
-    pub fn image_test(&self, image_path: &str) {
+    pub async fn image_test(&self, image_path: &str) -> std::io::Result<()> {
+        //let file = File::open(image_path).unwrap();
+        //let file_size = file.metadata().unwrap().len();
+        let url = format!(
+            "https://storage.googleapis.com/upload/v1beta/files?key={0}",
+            std::env::var("GEMINI_API_KEY").unwrap()
+        );
 
+        let client = reqwest::Client::new();
+        let request = client
+            .request(Method::POST, url)
+            .header("X-Goog-Upload-Protocol", "resumable");
+
+        Ok(())
     }
 }
