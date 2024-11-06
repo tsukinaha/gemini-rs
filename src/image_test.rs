099a787d0456aece4a657ad241e0252195f56c87
Shuflduf <shuflduf@gmail.com>
2024-11-06 15:15:22 -0700

    more things

--- src/image_test.rs
+++ src/image_test.rs
@@ -1,10 +1,10 @@
 use reqwest::Method;
 
-use crate::Conversation;
+use crate::{Conversation, GeminiError};
 use std::fs::File;
 
 impl Conversation {
-    pub async fn image_test(&self, image_path: &str) -> std::io::Result<()> {
+    pub async fn image_test(&self, image_path: &str) -> Result<(), GeminiError>{
         //let file = File::open(image_path).unwrap();
         //let file_size = file.metadata().unwrap().len();
         let url = format!(
@@ -15,7 +15,12 @@ impl Conversation {
         let client = reqwest::Client::new();
         let request = client
             .request(Method::POST, url)
-            .header("X-Goog-Upload-Protocol", "resumable");
+            .header("X-Goog-Upload-Protocol", "resumable")
+            .build();
+
+        let http_response = client.execute(request?).await?;
+        let response_dict = json::parse(&http_response.text().await.unwrap())?;
+        println!("{0}", response_dict.dump());
 
         Ok(())
     }
