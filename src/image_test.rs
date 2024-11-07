72606cf8501f6d2de5b4e8a2a9053c03a1dc8f4b
Shuflduf <shuflduf@gmail.com>
2024-11-07 15:52:46 -0700

    more things

--- src/image_test.rs
+++ src/image_test.rs
@@ -5,10 +5,12 @@ use std::fs::File;
 
 impl Conversation {
     pub async fn image_test(&self, image_path: &str) -> Result<(), GeminiError>{
-        //let file = File::open(image_path).unwrap();
-        //let file_size = file.metadata().unwrap().len();
+        let file = File::open(image_path).unwrap();
+        let file_size = file.metadata().unwrap().len();
+        let file_type = image_path.rsplit_once('.').unwrap().1;
+        let mime_filetype = Self::get_mime_filetype(file_type);
         let url = format!(
-            "https://storage.googleapis.com/upload/v1beta/files?key={0}",
+            "https://generativelanguage.googleapis.com/upload/v1beta/files?key={0}",
             std::env::var("GEMINI_API_KEY").unwrap()
         );
 
@@ -16,6 +18,10 @@ impl Conversation {
         let request = client
             .request(Method::POST, url)
             .header("X-Goog-Upload-Protocol", "resumable")
+            .header("X-Goog-Upload-Command", "start")
+            .header("X-Goog-Upload-Header-Content-Length", file_size)
+            .header("X-Goog-Upload-Header-Content-Type", mime_filetype)
+            .header("Content-Type", "application/json")
             .build();
 
         let http_response = client.execute(request?).await?;
@@ -23,5 +29,18 @@ impl Conversation {
         println!("{0}", response_dict.dump());
 
         Ok(())
+    }
+
+    fn get_mime_filetype(input: &str) -> String {
+        const IMAGES: [&str; 3] = [
+            "jpeg",
+            "png",
+            "webp",
+        ];
+        if IMAGES.contains(&input) {
+            format!("images/{0}", input)
+        } else {
+            "a".to_string()
+        }
     }
 }
