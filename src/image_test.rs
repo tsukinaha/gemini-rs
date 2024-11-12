d68d0a1ee9ffc66cec13c79f312096ae0881cb12
Shuflduf <shuflduf@gmail.com>
2024-11-12 00:45:55 -0700

    rearanging and uploading image bytes

--- src/image_test.rs
+++ src/image_test.rs
@@ -1,11 +1,11 @@
 use reqwest::Method;
 
 use crate::{Conversation, GeminiError};
-use std::fs::File;
+use std::fs::{self, File};
 
 impl Conversation {
     pub async fn image_test(&self, image_path: &str) -> Result<(), GeminiError>{
-        let file = File::open(image_path).unwrap();
+        let file = File::open(image_path)?;
         let file_size = file.metadata().unwrap().len();
         let file_type = image_path.rsplit_once('.').unwrap().1;
         let mime_filetype = Self::get_mime_filetype(file_type);
@@ -15,19 +15,32 @@ impl Conversation {
         );
 
         let client = reqwest::Client::new();
-        let request = client
-            .request(Method::POST, url)
+
+        // Upload metadata to google servers
+        let metadata_request = client
+            .request(Method::POST, &url)
             .header("X-Goog-Upload-Protocol", "resumable")
             .header("X-Goog-Upload-Command", "start")
             .header("X-Goog-Upload-Header-Content-Length", file_size)
             .header("X-Goog-Upload-Header-Content-Type", mime_filetype)
             .header("Content-Type", "application/json")
-            .body(r#""file": {"display_name": ""#.to_owned() + image_path.rsplit("/").last().unwrap() + r#""}}"#)
-            .build();
+            .body(r#""file": {"display_name": ""#.to_owned() + image_path.rsplit("/").last().unwrap() + "}}")
+            .send();
 
-        let http_response = client.execute(request?).await?;
-        let response_dict = json::parse(&http_response.text().await.unwrap())?;
-        println!("{0}", response_dict.dump());
+        println!("{0:?}", metadata_request.await.unwrap());
+
+        // Upload the actual bytes
+        let bytes_request = client
+            .request(Method::POST, &url)
+            .header("Content-Length", file_size)
+            .header("X-Goog-Upload-Offset", 0)
+            .header("X-Goog-Upload-Command", "upload, finalize")
+            .body(fs::read(image_path).unwrap())
+            .send();
+
+        println!("{0:?}", bytes_request.await.unwrap());
+
+
 
         Ok(())
     }
