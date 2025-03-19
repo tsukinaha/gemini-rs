7c2e5ca083810374cf30cbc6aaa8eea567960962
gvozdvmozgu <gvozdvmozgu@gmail.com>
2025-03-19 07:13:33 -0700

    replaced `gemini-1.5-flash` with `gemini-2.0-flash`

--- examples/ask.rs
+++ examples/ask.rs
@@ -2,7 +2,7 @@
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!(
         "{}",
-        gemini_rs::chat("gemini-1.5-flash")
+        gemini_rs::chat("gemini-2.0-flash")
             .send_message("Explain how AI works")
             .await?
     );
